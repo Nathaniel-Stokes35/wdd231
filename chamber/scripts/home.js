0ee43be32f2b06a43a820991829dbb843fc68b64
Nathaniel-Stokes <Nathaniel.Stokes35@Gmail.com>
2025-08-01 12:27:45 -0500

    removed "events" definition from Home.js

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -5,17 +5,6 @@ const currWeatherDiv = document.getElementById('curr-weather');
 const apiKey = '5ed0e22fbc6848ee0a15a76a220f8912';
 const city = "Pineville,US";
 
-const events = [
-  { "priority": 1, "title": "Power Outage Expected", "message": "Massive power outages are expected due to line maintenance and scattered thunderstorms this weekend. Residents are encouraged to prepare.", "date": "2025-08-02" },
-  { "priority": 1, "title": "Boil Water Advisory", "message": "A citywide boil water advisory has been issued due to a main break near downtown Anderson.", "date": "2025-08-01" },
-  { "priority": 3, "title": "Library Hours Update", "message": "The McDonald County Library will be closing an hour early this Friday for staff development.", "date": "2025-08-02" },
-  { "priority": 4, "title": "Farmers Market This Weekend", "message": "Don't forget to stop by the farmers market at the square Saturday morning for fresh produce and local crafts!", "date": "2025-08-03" },
-  { "priority": 2, "title": "Road Construction on Hwy 76", "message": "Expect delays on Hwy 76 near Tiff City as resurfacing work continues through the end of next week.", "date": "2025-08-04" },
-  { "priority": 4, "title": "Chamber Meeting", "message": "There will be a Chamber Meeting at the community center Saturday starting at 10:00am.", "date": "2025-08-03" },
-  { "priority": 3, "title": "T-Ball Game Delay", "message": "Farmers market will cause Saturday's T-Ball game for the Pineville Tornadoes to be delayed to accommodate facilities.", "date": "2025-08-03" },
-  { "priority": 2, "title": "Exit Closed on 49", "message": "Closed southbound exit 7 on 49, find alternate route", "date": "2025-07-25"}
-];
-
 function handleSpotlight(members) {
 
     if (!spotlightCont) return;
